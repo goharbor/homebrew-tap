316e9f6baec4a598cea811c2b9eb471bf22669eb
bupd <bupdprasanth@gmail.com>
2024-12-29 23:06:50 +0530

    add harbor cli formula
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ Forumula/harbor-cli.rb
@@ -0,0 +1,26 @@
+class Harbor < Formula
+  desc "[Sandbox] Official Harbor CLI"
+  homepage "https://github.com/goharbor/harbor-cli"
+  url "https://github.com/goharbor/harbor-cli/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "8a764ea10173d8dca4c99c79f55b054bea184abf012e7a346b7b39fae094d805"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+
+    ldflags = %W[
+      -s -w
+      -X github.com/goharbor/harbor-cli/cmd/harbor/internal/version.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/harbor"
+  end
+
+  test do
+    assert_match "#{version}", shell_output("#{bin}/harbor version")
+    # Test for handling credential store error during project creation.
+    assert_match "failed to read credential store", shell_output("#{bin}/harbor create project 2>&1", 1)
+  end
+
+end
