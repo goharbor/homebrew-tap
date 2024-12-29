909a81503047a464150ca7e5187da246d5b3a1c6
bupd <bupdprasanth@gmail.com>
2024-12-29 23:15:57 +0530

    update formula
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- Forumula/harborcli.rb
+++ Forumula/harborcli.rb
@@ -1,4 +1,4 @@
-class Harbor < Formula
+class Harborcli < Formula
   desc "[Sandbox] Official Harbor CLI"
   homepage "https://github.com/goharbor/harbor-cli"
   url "https://github.com/goharbor/harbor-cli/archive/refs/tags/v0.0.2.tar.gz"
