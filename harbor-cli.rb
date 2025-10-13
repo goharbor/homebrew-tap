edf4cb1c774b8b4e8d83339692633516a4700514
NucleoFusion <lakshit.singh.mail@gmail.com>
2025-10-13 09:37:49 +0530

    update: updating for goharbor/harbor-cli
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>
    
    fix: renaming to use checksums.txt
    
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>
    
    fix: name mismatch
    
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>
    
    updating for harbor-cli v0.0.12
    
    updating for harbor-cli v0.0.14
    
    updating for harbor-cli v0.0.15

--- harbor-cli.rb
+++ harbor-cli.rb
@@ -1,20 +1,20 @@
 class HarborCli < Formula
   desc "Harbor CLI for interacting with Harbor registry"
   homepage "https://goharbor.io"
-  version "0.0.12"
+  version "v0.0.15"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bupd/harbor-cli/releases/download/v0.0.12/harbor-cli_0.0.12_darwin_amd64.tar.gz"
-      sha256 "1dfc71f9ed1cd8cc7b2cb7593f67a6be98918f637972ac2bca510fc3f4ed0783" # amd64
+      url "https://github.com/goharbor/harbor-cli/releases/download/v0.0.15/harbor-cli_v0.0.15_darwin_amd64.tar.gz"
+      sha256 "" # amd64
 
       def install
         bin.install "harbor"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/bupd/harbor-cli/releases/download/v0.0.12/harbor-cli_0.0.12_darwin_arm64.tar.gz"
-      sha256 "43e44daf231af6025b8faa1ebfb6874ad07e0860ffab60e9caf6b5e9cc499e71" # arm64
+      url "https://github.com/goharbor/harbor-cli/releases/download/v0.0.15/harbor-cli_v0.0.15_darwin_arm64.tar.gz"
+      sha256 "" # arm64
 
       def install
         bin.install "harbor"
